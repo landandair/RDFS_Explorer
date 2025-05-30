c5a0a2d85681910779414503de0f346d213dc704
land_and_air <jonhuwaldt@gmail.com>
2025-05-29 22:22:34 -0500

    - Added node info, source info, and file retrieval - Added file dialog for saving file

--- rdfs-explorer/Scripts/connection_manager.gd
+++ rdfs-explorer/Scripts/connection_manager.gd
@@ -5,9 +5,13 @@ var http = HTTPClient.new()
 var host = '127.0.0.1'
 var port = 1234
 
+@onready var file_dialog = $FileDialog
+
 signal source_upate(source_dict: Dictionary)
 signal nodes_list(node_dict: Dictionary)
 signal node_info_received(node_dict: Dictionary)
+
+var current_file_data = PackedByteArray()
 
 func _ready() -> void:
 	var err = http.connect_to_host(host, port)
@@ -19,7 +23,7 @@ func _ready() -> void:
 			await get_tree().process_frame
 			
 		assert(http.get_status() == HTTPClient.STATUS_CONNECTED)
-
+	get_file('3e164dcd7800653828e2844cedcca00c7495c6afd78e00a3704be852')
 
 func _process(delta: float) -> void:
 	http.poll()
@@ -30,10 +34,42 @@ func _process(delta: float) -> void:
 func get_root() -> void:
 	var res = self.make_req('/')
 	if res == OK:
+		await self.get_headers()
 		var bin = await self.get_response_body()
 		print(bin.get_string_from_ascii())
 
+# Get info about a node from the server in json format
+func get_info(node_id: String) -> void:
+	var res = self.make_req('/getNode/{node_id}'.format({"node_id": node_id}))
+	if res == OK:
+		await self.get_headers()
+		var bin = await self.get_response_body()
+		var out = Dictionary(JSON.parse_string(bin.get_string_from_utf8()))
+		print("Make a signal that broadcasts ", out)
 
+# Get source information about local server to know which nodes are ours
+func get_source() -> void:
+	var res = self.make_req('/getSrc')
+	if res == OK:
+		await self.get_headers()
+		var bin = await self.get_response_body()
+		var out = bin.get_string_from_utf8()
+		print("Make a signal that broadcasts ", out)
+
+# Get file from server make a prompt to save it
+func get_file(node_id: String) -> void:
+	var res = self.make_req('/getFile/{node_id}'.format({"node_id": node_id}))
+	if res == OK:
+		var headers = await self.get_headers()
+		var bin = await self.get_response_body()
+		if 'Content-Disposition' in headers:
+			var regex = RegEx.new()
+			regex.compile("attachment; filename=(.+)")
+			var result = regex.search(headers['Content-Disposition'])
+			var file_name = result.get_string(1)  # None means no response
+			file_dialog.current_file = file_name
+			current_file_data = bin
+			file_dialog.show()
 
 # Make an http form and send it to the http peer for upload for uploading a file
 func upload_data(file_name: String, file_data: PackedByteArray, parent: String) -> void:
@@ -57,6 +93,7 @@ func upload_data(file_name: String, file_data: PackedByteArray, parent: String) 
 	body = body.get_string_from_utf8()
 	var res = self.make_post('/uploadData', headers, body)
 	if res == OK:
+		await self.get_headers()
 		var bin = await self.get_response_body()
 		print(bin.get_string_from_ascii())
 
@@ -80,6 +117,7 @@ func make_directory(name, parent: String) -> void:
 	body = body.get_string_from_utf8()
 	var res = self.make_post('/mkdir', headers, body)
 	if res == OK:
+		await self.get_headers()
 		var bin = await self.get_response_body()
 		print(bin.get_string_from_ascii())
 
@@ -88,6 +126,7 @@ func remove_node(id: String) -> void:
 	print('/deleteNode/%s' % id)
 	var res = self.make_req('/deleteNode/%s' % id)
 	if res == OK:
+		await self.get_headers()
 		var bin = await self.get_response_body()
 		print(bin.get_string_from_ascii())
 
@@ -104,27 +143,31 @@ func make_post(url, header=['Content-type: json'], body='') -> Error:
 	return ERR_CANT_CONNECT
 
 func get_response_body() -> PackedByteArray:
-	while http.get_status() == HTTPClient.STATUS_REQUESTING:
-		# Keep polling for as long as the request is being processed.
-		await get_tree().process_frame
+	# ensure you ran get_headers before this
 	var rb = PackedByteArray() # Array that will hold the data.
-	if http.has_response():
-		# If there is a response...
-		var headers = http.get_response_headers_as_dictionary() # Get response headers.
-		#print("code: ", http.get_response_code()) # Show response code.
-		#print("**headers:\\n", headers) # Show headers.
-		while http.get_status() == HTTPClient.STATUS_BODY:
-			# While there is body left to be read
-			http.poll()
-			# Get a chunk.
-			var chunk = http.read_response_body_chunk()
-			if chunk.size() == 0:
-				await get_tree().process_frame
-			else:
-				rb = rb + chunk # Append to read buffer.
+	# If there is a response...
+	while http.get_status() == HTTPClient.STATUS_BODY:
+		# While there is body left to be read
+		http.poll()
+		# Get a chunk.
+		var chunk = http.read_response_body_chunk()
+		if chunk.size() == 0:
+			await get_tree().process_frame
+		else:
+			rb = rb + chunk # Append to read buffer.
 		# Done!
 		var text = rb.get_string_from_ascii()
 	return rb
+
+func get_headers() -> Dictionary:
+	var headers = {}
+	while http.get_status() == HTTPClient.STATUS_REQUESTING:
+		# Keep polling for as long as the request is being processed.
+		await get_tree().process_frame
+	if http.has_response():
+		# If there is a response...
+		headers = http.get_response_headers_as_dictionary() # Get response headers.
+	return headers
 
 func make_multipart_header():
 	# Create some random bytes to generate our boundary value
@@ -146,3 +189,11 @@ func append_line(buffer:PackedByteArray, line:String) -> void:
 func append_bytes(buffer:PackedByteArray, bytes:PackedByteArray) -> void:
 	buffer.append_array(bytes)
 	buffer.append_array('\r\n'.to_utf8_buffer())
+
+# Save current file data to file
+func _on_file_dialog_file_selected(path: String) -> void:
+	if len(current_file_data):
+		var f = FileAccess.open(path, FileAccess.WRITE)
+		f.store_buffer(current_file_data)
+		f.close()
+		current_file_data = PackedByteArray()
